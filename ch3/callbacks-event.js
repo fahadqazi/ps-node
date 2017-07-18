7bf78e42316dbdb44d5d4c385003f89815e71787
Fahad Qazi <fahadqazi@gmail.com>
2017-07-18 20:48:05 +0100

    example tutorial

--- ch3/callbacks-event.js
+++ ch3/callbacks-event.js
@@ -1,17 +1,35 @@
 /**
  * callbacks vs Events
  * 
- * Callback
+ * Callback - Request/Reply 
+ * You make a request and set a function to be called when request completes
+ * One request one reply
+ * No result until you get all the result
+ * callback function wates for all items to arrived before running
+ * getThem function is accumulating and storing everything along the way
+ * All or nothing proposition
  * 
  * getSomething('item', function(){
  *  console.log('done')
  * })
  * 
- * Events
+ * Events - Publish/Subscribe
+ * 'on' function can be invoked repeatedly 
+ * function associated with an event run when that event is triggered
+ * Not accumulating items in memory
  * 
  * var results = getSomething(param)
  * 
  * results.on('item', function(){})
  * results.on('done', function(){})
  * results.on('error', function(){})
+ */
+
+ /** 
+ The Publisher                            The Subscriber
+ emitter.emit(event, [args]) --->         emitter.on(event, listener)
+ 
+ 
+
+
  */
